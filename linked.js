fe6fb2bd1daedbb3c8938d74b5c89ebc1c472a15
zaid-ali <zaid.ali5430@gmail.com>
2021-03-19 19:57:27 +0500

    add getUnlinkedUsers method

--- linked.js
+++ linked.js
@@ -1,1 +1,16 @@
 // find all the users that have not been linked
+const { allUsers, linked } = require('./users')
+
+// console.log(linked)
+
+function getUnlinkedUsers(allUsers, linked) {
+	let unlinkedUsers = []
+	allUsers.map((user) => {
+		if (!linked.some((linkedUser) => linkedUser.id === user.id)) {
+			unlinkedUsers.push(user)
+		} 
+	})
+	console.log(unlinkedUsers)
+}
+
+getUnlinkedUsers(allUsers, linked)
